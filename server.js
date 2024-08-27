fca1e28206475fedaf71428ade80ffda448518fe
MatthewSidoti <coding.sidoti@gmail.com>
2024-08-26 20:55:01 -0400

    8/26/24

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const applicationsController = require('./controllers/application.js');
 
 const authController = require('./controllers/auth.js');
-
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -27,22 +31,20 @@ app.use(
     saveUninitialized: true,
   })
 );
-
+app.use(passUserToView);
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+
 
 app.use('/auth', authController);
+app.use(isSignedIn);
+
+app.use('/user/:userId/applications', applicationsController);
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
